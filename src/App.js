3b03fd75216a64a1fad6277be1c63eaa7fe2ed3f
Adam Purcell <apurcell88@gmail.com>
2023-06-16 15:10:24 -0500

    Added authentication through Google and added styling.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
+import Auth from './components/Auth';
+import  { auth }  from './config/firebase';
 
 function App() {
   // state management
-  const [auth, isAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
 
+  if (!isAuth) {
+    return (
+      <Auth
+        setIsAuth={setIsAuth}
+      />
+    )
+  }
+
+  console.log(auth)
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <h1>yay</h1>
+  )
 }
 
 export default App;
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,24 +4,30 @@ import Cookies from 'universal-cookie';
 
 const cookie = new Cookies();
 
-const Auth = () => {
+const Auth = (props) => {
+  const { setIsAuth } = props;
+
   const signInWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
       cookie.set('auth-token', result.user.refreshToken);
+      setIsAuth(true);
     } catch(err) {
         console.error(err);
     }
   }
 
   return (
-    <div>
-      <p>Sign in with Google</p>
-      <button
-        onClick={signInWithGoogle}
-      >
-        Sign In
-      </button>
+    <div className="sign-in-container">
+      <div className="sign-in-info">
+        <p>Please Sign In</p>
+        <button
+          className="sign-in-btn"
+          onClick={signInWithGoogle}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   )
 }
